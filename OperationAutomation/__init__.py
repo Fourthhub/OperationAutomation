0e38606a0d91136a76e0a4eb18fde20a928d1f60
diego <diegoechaure@gmail.com>
2024-03-30 23:34:18 +0100

    rase deleted

--- OperationAutomation/__init__.py
+++ OperationAutomation/__init__.py
@@ -83,7 +83,6 @@ def moverLimpiezasConSusIncidencias(propertyID,token):
 
 
 # Iterar a través de las tareas y añadir  la prioridad de cada una
-    raise ValueError(json.dumps(response))
     for tarea in response["results"]:
         estado = tarea["type_task_status"]["name"]
         if estado not in ["Finished", "Closed","In-Progress"]:
