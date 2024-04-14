a85d22c4fe1b4e2fd422ecdb2ad6b29d4a6ec63c
diego <diegoechaure@gmail.com>
2024-04-14 22:47:01 +0200

    fdswdsfwe

--- function_app.py
+++ function_app.py
@@ -171,7 +171,7 @@ def conseguirPropiedades(token):
     return response.json()
 
 @app.function_name(name="operationautomation")
-@app.schedule(schedule="0 30 20 * * *", 
+@app.schedule(schedule="0 0 21 * * *", 
               arg_name="myTimer",
               run_on_startup=False) 
 def operationautomation(myTimer: func.TimerRequest) -> None:
